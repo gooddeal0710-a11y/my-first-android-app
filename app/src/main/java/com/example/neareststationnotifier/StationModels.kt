8d28681225ee762e7b0458419aca7ca6e4754d7e
gooddeal0710-a11y <gooddeal0710@gmail.com>
2026-03-06 00:49:51 +0900

    Create StationModels.kt

---
+++ app/src/main/java/com/example/neareststationnotifier/StationModels.kt
@@ -0,0 +1,10 @@
+package com.example.neareststationnotifier
+
+data class StationCandidate(
+    val name: String,
+    val line: String,
+    val company: String,
+    val distanceRaw: String,
+    val next: String,
+    val prev: String
+)
